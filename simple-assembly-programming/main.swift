6e0a20ce149459587bfcef6e1cebc3c214178420
Ian Huang <ianhuang46@gmail.com>
2019-03-22 15:46:05 -0400

    Xcode project init

---
+++ simple-assembly-programming/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  simple-assembly-programming
+//
+//  Created by Ian Huang on 3/22/19.
+//  Copyright © 2019 Ian Huang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
