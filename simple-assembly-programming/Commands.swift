64df3e1effd8c0cfbde21c01227a0ae629fbd3e0
SweetenedIcedTea <ethanzhang08@yahoo.com>
2019-03-24 22:20:50 -0400

    I have finished my portion of the project
    I am done

---
+++ simple-assembly-programming/Commands.swift
@@ -0,0 +1,13 @@
+//
+//  Commands.swift
+//  simple-assembly-programming
+//
+//  Created by Ethan Zhang on 3/24/19.
+//  Copyright © 2019 Ian Huang. All rights reserved.
+//
+
+import Foundation
+
+enum Commands{
+    
+}
---
+++ simple-assembly-programming/OracleVirtualBox.swift
@@ -0,0 +1,15 @@
+//
+//  OracleVirtualBox.swift
+//  simple-assembly-programming
+//
+//  Created by Ethan Zhang on 3/24/19.
+//  Copyright © 2019 Ian Huang. All rights reserved.
+//
+
+import Foundation
+
+class VM{
+    var inteli7 = CPU<Int64>(memSize: 420, numRegisters: 69)
+    
+    
+}
