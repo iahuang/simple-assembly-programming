8cd6a7c04204b81001842e02d993abdc4b8344f3
SweetenedIcedTea <ethanzhang08@yahoo.com>
2019-04-10 23:05:45 -0400

    yes
    yes

--- simple-assembly-programming/instructions.swift
+++ simple-assembly-programming/instructions.swift
@@ -12,71 +12,87 @@ extension CPU {
     func clr(target: Ref) {
         target.value = 0
     }
+    
     func mov(src: Ref, dest: Ref) {
         dest.value = src.value
     }
+    
     func add(src: Ref, dest: Ref) {
         dest.value += src.value
     }
+    
     func sub(src: Ref, dest: Ref) {
         dest.value -= src.value
     }
+    
     func mul(src: Ref, dest: Ref) {
         dest.value *= src.value
     }
+    
     func div(src: Ref, dest: Ref) {
         dest.value /= src.value
     }
+    
     func jmp(to: Ref) {
         rpc = to.value - 1
     }
+    
     func sojz(target: Ref, jump: Ref) {
         target.value -= 1
         if target.value == 0 {
             jmp(to: jump)
         }
     }
+    
     func sojnz(target: Ref, jump: Ref) {
         target.value -= 1
         if target.value != 0 {
             jmp(to: jump)
         }
     }
+    
     func aojz(target: Ref, jump: Ref) {
         target.value += 1
         if target.value == 0 {
             jmp(to: jump)
         }
     }
+    
     func aojnz(target: Ref, jump: Ref) {
         target.value += 1
         if target.value != 0 {
             jmp(to: jump)
         }
     }
+    
     func cmp(a: Ref, b: Ref) {
         rcp = b.value-a.value
     }
+    
     func jmpn(to: Ref) {
         if rcp < 0 {
             jmp(to: to)
         }
     }
+    
     func jmpne(to: Ref){
         if rcp != 0{
             jmp(to: to)
         }
     }
+    
     func jmpz(to: Ref) {
         if rcp == 0 {
             jmp(to: to)
         }
     }
+    
     func jmpp(to: Ref) {
         if rcp > 0 {
             jmp(to: to)
         }
     }
+    
     func jsr(to: Ref) {
         stackPush(rpc)
         jmp(to: to)
@@ -86,6 +102,7 @@ extension CPU {
         }
         
     }
+    
     func ret() {
         let returnTo = srStack.popLast()!
         jmp(to: ConstantReference(self, returnTo))
@@ -93,18 +110,23 @@ extension CPU {
             reg[regNum] = stackPop()
         }
     }
+    
     func push(n: Ref) {
         stackPush(n.value)
     }
+    
     func pop(into: Ref) {
         into.value = stackPop()
     }
+    
     func outc(char: Ref)-> String{
         return(String(unicodeValueToCharacter(char.value)))
     }
+    
     func printi(int: Ref)-> String{
         return("\(Int(int.value))")
     }
+    
     func outs(label: MemoryReference)-> String{
         let dataStart = label.addr
         let stringLength = Int(get(dataStart))
@@ -117,4 +139,20 @@ extension CPU {
         return(result)
     }
     // outcr outs
+    
+    func stackc(into: RegisterReference){
+        if(rst == 100){
+            into.value = 1
+            return
+        }
+        if(rst == 0){
+            into.value = 2
+            return
+        }
+        into.value = 0
+    }
+    
+    func nop(){
+        return
+    }
 }
