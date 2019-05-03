1a8c41341fb3594b3ee33c61296548ed5841185a
SweetenedIcedTea <ethanzhang08@yahoo.com>
2019-05-02 22:46:03 -0400

    hentai
    hentai

--- simple-assembly-programming/OracleVirtualBox.swift
+++ simple-assembly-programming/OracleVirtualBox.swift
@@ -10,7 +10,7 @@
  import Foundation
 
  class VM{
-    var inteli7 = CPU(memSize: 1000, numRegisters: 100)
+    var inteli7 = CPU(memSize: 69420, numRegisters: 420)
     //var disasm = Disassembler()
     var quit = false
 
