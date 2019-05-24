d3ee5b39f0e6c5f590178efe1275729c4205a9e0
Ian Huang <ianhuang46@gmail.com>
2019-05-24 01:30:25 -0400

    Prueba este

--- simple-assembly-programming/Assembler.swift
+++ simple-assembly-programming/Assembler.swift
@@ -79,12 +79,14 @@ func buildArgTable() -> [String:[String]] {
         
         for i in 1...parts.count-1 {
             let suffix = parts[i]
+            let mne = base+String(suffix)
             var argtypes = suffix.map { String($0) }
-            
-            if ["aoj", "soj", "mp"].contains(base) {
+            if mne == "movb" {
+                argtypes = ["r", "r", "r"]
+            } else if ["aoj", "soj", "mp"].contains(base) {
                 argtypes = ["m"]
             } else if suffix == "b" {
-                argtypes = ["r", "r", "r"]
+                argtypes = ["r", "r"]
             }
             
             argtable[base+String(suffix)] = argtypes
